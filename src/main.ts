0cc3e0776c5567c260a4d2f9225c6c68b7e829e8
BurakKontas <aburakkontas@trakya.edu.tr>
2023-07-12 05:37:33 +0300

    Ne yaptım bilmiyorum sadece pushluyorum

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
 
 async function bootstrap() {
-    dotenv.config(); // .env dosyasını yükle
+    dotenv.config();
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: ["*"], // TODO: Change this to the actual origin
+    });
     await app.listen(5050);
 }
 bootstrap();
